57266e26dcd15b271c6466cab97910c77b991502
nidal197171 <Ned.sfo@gmail.com>
2026-09-23 17:46:34 +0400

    Add top-of-site disclaimer bar: global ground transportation arranger, not a limo company

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1098,6 +1098,7 @@ function Index() {
 
   return (
     <>
+      <div className="disclaimer-bar">We are a global ground transportation arranger (we are not a limo company)</div>
       <div className="announce-bar">Coming soon to more US cities and Dubai</div>
       <header>
         <div className="wrap nav-inner">
